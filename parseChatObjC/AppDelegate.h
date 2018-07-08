66e3bb8e159aa97172b15059d79b26c02de4c430
Jigyasaa Sood <jsood@ucsd.edu>
2018-07-08 14:14:14 -0700

    first commit

---
+++ parseChatObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  parseChatObjC
+//
+//  Created by jsood on 7/8/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ parseChatObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  parseChatObjC
+//
+//  Created by jsood on 7/8/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
